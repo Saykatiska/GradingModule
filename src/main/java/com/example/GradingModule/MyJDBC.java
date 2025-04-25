f3463eb0e47eb5166c4aacdee7ee80377dd3ee34
brgyplrm <borgypalermo@gmail.com>
2025-04-25 11:56:57 +0800

    Db connection

--- src/main/java/com/example/GradingModule/MyJDBC.java
+++ src/main/java/com/example/GradingModule/MyJDBC.java
@@ -3,19 +3,25 @@ package com.example.GradingModule;
 import java.sql.*;
 
 public class MyJDBC {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        String url = "jdbc:postgresql://aws-0-ap-southeast-1.pooler.supabase.com:6543/postgres";
+        String user = "postgres.odyfrnuddvhbedvjfnhw";
+        String password = "HelloWorld123!";
 
-        try {
-            Connection connection = DriverManager.getConnection(
-                    "jdbc:postgresql://aws-0-ap-southeast-1.pooler.supabase.com:6543/postgres",
-                    "postgres.odyfrnuddvhbedvjfnhw",
-                    "HelloWorld123!"
-            );
+        try (Connection connection = DriverManager.getConnection(url, user, password);
+             Statement countStatement = connection.createStatement();
+             ResultSet countResult = countStatement.executeQuery("SELECT COUNT(*) FROM grades");
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery("SELECT * FROM grades")) {
 
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM grades");
+            int rowCount = 0;
+            if (countResult.next()) {
+                rowCount = countResult.getInt(1);
+            }
+            System.out.println("Total number of records: " + rowCount);
+            System.out.println();
 
-            while(resultSet.next()){
+            while (resultSet.next()) {
                 String studentId = resultSet.getString("studid");
                 String subjectCode = resultSet.getString("subjcode");
                 String finalGrade = resultSet.getString("fingrade");
@@ -25,8 +31,11 @@ public class MyJDBC {
                 System.out.println("Subject Code: " + subjectCode);
                 System.out.println("Final Grade: " + finalGrade);
                 System.out.println("Grade Status: " + gradeStatus);
+                System.out.println();
             }
-        }catch (SQLException e) {
+
+        } catch (SQLException e) {
+            System.err.println("Database error occurred:");
             e.printStackTrace();
         }
     }
