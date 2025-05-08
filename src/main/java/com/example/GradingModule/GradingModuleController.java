c9510a050290b1ff3ef8853688211a3477346df5
Saykatiska <cydtomas06@gmail.com>
2025-05-08 19:01:11 +0800

    Add revised FXML file (fixed width) and Grading Module Controller

---
+++ src/main/java/com/example/GradingModule/GradingModuleController.java
@@ -0,0 +1,22 @@
+package com.example.GradingModule;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class GradingModuleController implements Initializable {
+    
+    @FXML
+    private AnchorPane anchorPane;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // This method is called after all @FXML annotated members have been injected
+        // Initialize your controller logic here
+    }
+    
+    // Add your control methods here
+}
