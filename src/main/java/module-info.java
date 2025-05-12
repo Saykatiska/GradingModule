b3dd3b78f1fecb6ce0ae3d19cc1a91f08e9b1a46
brgyplrm <borgypalermo@gmail.com>
2025-05-12 15:26:41 +0800

    linking

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,12 +2,9 @@ module com.example.gradingmodule {
     requires javafx.controls;
     requires javafx.fxml;
     requires org.postgresql.jdbc;
-    requires com.dlsc.formsfx;
     requires java.sql;
     requires java.desktop;
 
     opens com.example.GradingModule to javafx.fxml;
-    opens com.example.dbconnection;
     exports com.example.GradingModule;
-    exports com.example.dbconnection;
 }
